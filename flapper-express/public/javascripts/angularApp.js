1d021ab93a16e3fde71122fd83ecace4232324da
Pieter-Jan Vandenberghe <pieterjan.vandenberghe.t6365@student.hogent.be>
2015-11-22 19:52:39 +0100

    Added slideshow

--- flapper-express/public/javascripts/angularApp.js
+++ flapper-express/public/javascripts/angularApp.js
@@ -1,4 +1,4 @@
-var app = angular.module('flapperNews', ['ui.router']);
+var app = angular.module('flapperNews', ['ui.router', 'ngAnimate']);
 
 app.controller('MainCtrl', [
   '$scope',
@@ -108,6 +108,54 @@ app.controller('NavCtrl', [
     $scope.logOut = auth.logOut;
   }
 ]);
+
+app.controller('SliderController', function($scope) {
+  $scope.images = [{
+    src: 'img1.png',
+    title: 'Pic 1'
+  }, {
+    src: 'img2.jpg',
+    title: 'Pic 2'
+  }, {
+    src: 'img3.jpg',
+    title: 'Pic 3'
+  }, {
+    src: 'img4.png',
+    title: 'Pic 4'
+  }, {
+    src: 'img5.png',
+    title: 'Pic 5'
+  }];
+});
+
+app.directive('slider', function($timeout) {
+  return {
+    restrict: 'AE',
+    replace: true,
+    scope: {
+      images: '='
+    },
+    link: function(scope, elem, attrs) {
+      scope.currentIndex = 0; // Initially the index is at the first image
+
+      scope.next = function() {
+        scope.currentIndex < scope.images.length - 1 ? scope.currentIndex++ : scope.currentIndex = 0;
+      };
+
+      scope.prev = function() {
+        scope.currentIndex > 0 ? scope.currentIndex-- : scope.currentIndex = scope.images.length - 1;
+      };
+      scope.$watch('currentIndex', function() {
+        scope.images.forEach(function(image) {
+          image.visible = false; // make every image invisible
+        });
+
+        scope.images[scope.currentIndex].visible = true; // make the current image visible
+      });
+    },
+    templateUrl: 'templates/templateurl.html'
+  };
+});
 
 app.config([
   '$stateProvider',
